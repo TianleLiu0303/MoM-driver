e7d29f1e47d69ec027c923d597b3f662d97c01fc
TianleLiu0303 <987981443@qq.com>
2026-04-06 17:06:38 +0800

    init paper architecture

--- debug.py
+++
@@ -1,6 +0,0 @@
-import pickle
-import gzip
-with gzip.open('/home/gpus-09/nanyi/rwkv/exp/nanyi_trainning_cache_20_padding/2021.06.14.18.13.35_veh-26_00385_00471/90f1f4ebc0765656/transfuser_feature.gz', 'rb') as f:
-    data_points = pickle.load(f)
-
-print(data_points)
